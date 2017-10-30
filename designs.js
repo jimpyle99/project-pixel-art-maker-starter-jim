f3cd58eaffb2f06d43ae1706f5ae0e54a90c0c7c
Jim Pyle <jimpyle@gmail.com>
2017-10-30 17:21:31 -0500

    n by m gird for loop i think

--- designs.js
+++ designs.js
@@ -2,9 +2,14 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+row = ('#height');
+col = ('#width');
+container = ('#pixel_canvas');
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(function makeGrid() {
+  for (var n = 0; i < row; y++){
+      for (var m = 0; i < col; x++){
+        unit.appendTo('container');
+    }
+  }
+})
