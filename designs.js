8c0d31c0db5dd68543bd5e5ed637542cfcbfdbb0
Jim Pyle <jimpyle@gmail.com>
2017-10-30 19:05:57 -0500

    got new direction from udaicy chat support - row and col

--- designs.js
+++ designs.js
@@ -2,14 +2,21 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var row,col,container,n,m, changeColor;
+
 row = ('#height');
 col = ('#width');
-container = ('#pixel_canvas');
+container = ('table','#pixel_canvas');
+makeGird = ('#pixel_canvas');
 
-$(function makeGrid() {
-  for (var n = 0; i < row; y++){
-      for (var m = 0; i < col; x++){
-        unit.appendTo('container');
+function makeGrid(row,col) {
+  for (var n = 0; n < row; n++){
+      for (var m = 0; m < col; m++){
+        unit.appendTo('parent');
     }
   }
+}
+
+$(input).on('click',function changeColor()){
+  toggle.used.color.on.grid;
 })
