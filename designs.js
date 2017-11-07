1b0d89de3c88f9d9b8d2185a81b13916cc7c9784
jimpyle99 <jimpyle@gmail.com>
2017-11-07 08:56:42 -0600

    got some help and major changes

--- designs.js
+++ designs.js
@@ -1,30 +1,48 @@
 // Select color input
-// Select size input
+// 3Select size input
 
 // When size is submitted by the user, call makeGrid()
-var row,col,container,n,m, changeColor;
+let gridForm = document.querySelector('#size-picker');
+let selectedColor = document.querySelector('#color-picker');
 
-container = ('table','#pixel_canvas');
-makeGird = ('#pixel_canvas');
-let table = document.getElementById('pixel_canvas');
+gridForm.addEventListener("submit", populateGrid);
 
-function saveData(){
-  event.preventDefault();
-  let height = document.getElementById('input_height').value;
-  let width = document.getElementById('input_width').value;
-  makeGrid(height, width);
+function clearTable(table) {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
 }
 
-document.getElementById('sizePicker').onsubmit = function(){
-  saveData();
+function colorCell(evt) {
+  let style = evt.srcElement.style;
+  if (!style.backgroundColor) {
+    style.backgroundColor = selectedColor.value;
+  } else {
+    style.backgroundColor = "";
+  }
 }
 
-function makeGrid(rows,cols) {
-  for (let row = 0; row < rows; row++){
-      let tr = table.insertRow();
-      for (let col = 0; col < cols; col++){
-        let cl = tr.insertCell();
-        cl.addEventListener('click', fillColor);
-      }
+function populateGrid(evt) {
+  let row, col, pixelCanvas, inputWidth, inputHeight;
+
+  pixelCanvas = document.querySelector('#pixel-canvas');
+  inputWidth = document.querySelector('#input-width').value;
+  inputHeight = document.querySelector('#input-height').value;
+  inputWidth = parseInt(inputWidth);
+  inputHeight = parseInt(inputHeight);
+
+  evt.preventDefault();
+
+  clearTable(pixelCanvas);
+
+  for (row = 0; row < inputHeight; row++) {
+    let tr = document.createElement("tr");
+
+    for (col = 0; col < inputWidth; col++) {
+      let td = document.createElement("td");
+      td.addEventListener("click", colorCell);
+      tr.appendChild(td);
+    }
+    pixelCanvas.appendChild(tr);
   }
 }
