a25fa4091e7bbe87c290a221a0a896e9e123cbd2
Jim Pyle <jimpyle@gmail.com>
2017-11-09 12:57:15 -0600

    added comments and udacity feedback after review

--- designs.js
+++ designs.js
@@ -1,18 +1,21 @@
 // Select color input
 // 3Select size input
 
-// When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, call gridForm()
 let gridForm = document.querySelector('#size-picker');
+// When color is submitted by the user, call (selectedColor)
 let selectedColor = document.querySelector('#color-picker');
 
 gridForm.addEventListener("submit", populateGrid);
 
+//clearTable will make sure grid is clear before drawing it
 function clearTable(table) {
   while (table.firstChild) {
     table.removeChild(table.firstChild);
   }
 }
 
+//colorCell will change cell background with the selectedColor or will make the background white if it already has a selectedColor
 function colorCell(evt) {
   let style = evt.srcElement.style;
   if (!style.backgroundColor) {
@@ -22,6 +25,7 @@ function colorCell(evt) {
   }
 }
 
+//populateGrid makes inputWidth and inputHeight intigers
 function populateGrid(evt) {
   let row, col, pixelCanvas, inputWidth, inputHeight;
 
@@ -31,10 +35,13 @@ function populateGrid(evt) {
   inputWidth = parseInt(inputWidth);
   inputHeight = parseInt(inputHeight);
 
+//prevents the default behavior of the submit button from resetting the page
   evt.preventDefault();
 
+//clearTable makes sure the grid is blank to prevent adding tr and td to existing grids
   clearTable(pixelCanvas);
 
+//loop that appends tr(row) and td(height) to pixelCanvas table
   for (row = 0; row < inputHeight; row++) {
     let tr = document.createElement("tr");
 
